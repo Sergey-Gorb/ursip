7e92a39d437847cc3df897e9a6631e7a72029ae6
Sergey-Gorb <mpost39228@mail.ru>
2023-04-26 11:29:43 +0300

    Add stat

--- xlsparser/urls.py
+++ xlsparser/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     # path('export', views.export_data, name='export_data'),
     path('import', views.xls_upload, name='import_data'),
+    path('stat', views.get_stat, name='get-stat'),
 ]
--- xlsparser/views.py
+++ xlsparser/views.py
@@ -1,6 +1,11 @@
+import datetime
+
+from django.db.models import Sum
+from django.http import JsonResponse
 from django.shortcuts import render
 from tablib import Dataset
 
+from xlsparser.models import XlsRow
 from xlsparser.resources import XlsRowResource
 
 
@@ -17,3 +22,26 @@ def xls_upload(request):
             row_resource.import_data(dataset, dry_run=False)  # Actually import now
 
     return render(request, 'xlsparser/xls_upload.html')
+
+
+def get_stat(request):
+    date_today = datetime.date.today()
+    if request.method == 'POST':
+        date_from = request.POST.get('date_from',date_today)
+        date_to = request.POST.get('date_to',date_today)
+        stat = XlsRow.objects.filter(
+            operdate_range=(date_from, date_to)
+        ).aggregate(
+            sum1=Sum('fact_qlig_data1'),
+            sum2=Sum('fact_qlig_data2'),
+            sum3=Sum('fact_qoil_data1'),
+            sum4=Sum('fact_qoil_data2'),
+            sum5=Sum('forecast_qlig_data1'),
+            sum6=Sum('forecast_qlig_data2'),
+            sum7=Sum('forecast_qoil_data1'),
+            sum8=Sum('forecast_qoil_data2')
+        ).values()
+        return JsonResponse({'status': 'ok', 'sum': stat})
+    else:
+        return JsonResponse({'status': 'error'})
+
